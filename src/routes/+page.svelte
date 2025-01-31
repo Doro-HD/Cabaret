f982ffef4d1582ddc5df6135ec07e615da34f983
doro-hd <doro-hd@protonmail.com>
2025-01-31 17:27:20 +0100

    Add grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { Button } from '$lib/shad/components/ui/button';
 </script>
 
-<div class="place-items-center space-y-2 pt-5">
+<div class="grid place-items-center space-y-2 pt-5">
 	<h1 class="text-5xl font-bold text-center">Welcome to Cabaret</h1>
 
 	<div>
